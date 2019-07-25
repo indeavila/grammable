4e64d71a525835965bc884d6daaac4b5161791ff
Ivan Nikolai De Avila <indeavila@gmail.com>
2019-07-25 22:47:35 +0000

    201907250550

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,6 +5,12 @@ class GramsController < ApplicationController
   end
   def index
   end
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
