0bf87fa1985fa0bbc30527f95a0112ddcd681bb5
Ivan Nikolai De Avila <indeavila@gmail.com>
2019-07-31 21:14:37 +0000

    201907310415

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,7 +7,8 @@ FactoryBot.define do
     password_confirmation { "secretPassword" }
   end
   factory :gram do
-    message { "hello"}
+    message { "hello" }
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
     association :user
   end
 end
